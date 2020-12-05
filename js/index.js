2c748a472649572ba199d555a6e82062eda5050d
ckpham2 <64879131+ckpham2@users.noreply.github.com>
2020-12-05 14:48:20 -0800

    UX_UI_HW_19

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 console.log("Your index.js file is loaded correctly!");
+
+function fade() {
+    $('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 3000);
+
+$('#buttonClick').on("click",function(){
+    $('.cssChange').css("background-color","#A9CDCA");
+    $('.cssChange').css("color","#1D1F1E");
+    $('.cssChange').css("box-shadow","0px 4px 4px grey");
+})
+
+$("li").on("hover",function(){
+    $('.buttonClick').mouseenter("color","#A9CDCA");
+});
