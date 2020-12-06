166fca4f250da721100af7ee326c9521bb1f2347
ckpham2 <64879131+ckpham2@users.noreply.github.com>
2020-12-05 19:33:25 -0800

    UX UI HW 19

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function fade() {
 setTimeout(fade, 3000);
 
 $('#buttonClick').on("click",function(){
-    $('.cssChange').css("background-color","#A9CDCA");
+    $('.cssChange').css("background-color","#4A807B");
     $('.cssChange').css("color","#1D1F1E");
     $('.cssChange').css("box-shadow","0px 4px 4px grey");
 })
